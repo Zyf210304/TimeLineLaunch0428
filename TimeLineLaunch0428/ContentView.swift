3cc773851e7a348364a9e542735fa4218708852f
张亚飞 <672233580@qq.com>
2022-04-28 14:27:22 +0800

    Initial Commit

---
+++ TimeLineLaunch0428/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  TimeLineLaunch0428
+//
+//  Created by 张亚飞 on 2022/4/28.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ TimeLineLaunch0428/TimeLineLaunch0428App.swift
@@ -0,0 +1,17 @@
+//
+//  TimeLineLaunch0428App.swift
+//  TimeLineLaunch0428
+//
+//  Created by 张亚飞 on 2022/4/28.
+//
+
+import SwiftUI
+
+@main
+struct TimeLineLaunch0428App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
